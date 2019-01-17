hopez13/art

ART: add libprocessgroup dependency and fix sched_policy include
Because art is using set_sched_policy which is moved into libprocessgroup
an additional dependency is added and header file change is made.

Bug: 111307099
Test: builds, boots

Change-Id: If23739013092361fd9f2f4a99cca617516ae06f3
Signed-off-by: Suren Baghdasaryan <surenb@google.com>

runtime/thread_android.cc
@@ -21,7 +21,7 @@
 #include <sys/resource.h>
 #include <sys/time.h>
 
-#include <cutils/sched_policy.h>
+#include <processgroup/sched_policy_ctrl.h>
 #include <utils/threads.h>
 
 #include "base/macros.h"

paschalis-mpeis/android-llvm

runtime/thread_android.cc
@@ -21,7 +21,7 @@
 #include <sys/resource.h>
 #include <sys/time.h>
 
-#include <cutils/sched_policy.h>
+#include <processgroup/sched_policy.h>
 #include <utils/threads.h>
 
 #include "base/macros.h"